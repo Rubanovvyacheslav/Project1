6789714548eceb6cd6b187a76877d41e19eb9931
Rubanov <Rubanov.work@gmail.com>
2023-05-14 15:53:34 +0300

    add Book & Author fix

--- src/lesson1/Author.java
+++ src/lesson1/Author.java
@@ -1,8 +1,8 @@
 package lesson1;
 
 public class Author {
-    String firstName;
-    String lastName;
+    private String firstName;
+    private String lastName;
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
