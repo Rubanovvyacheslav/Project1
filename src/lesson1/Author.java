8354c970f66f96b7e905313bec02ae394bb2ccf7
Rubanov <Rubanov.work@gmail.com>
2023-05-15 18:09:22 +0300

    add toString, equals & hashCode

--- src/lesson1/Author.java
+++ src/lesson1/Author.java
@@ -1,5 +1,7 @@
 package lesson1;
 
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String lastName;
@@ -17,7 +19,24 @@ public class Author {
         return this.lastName;
     }
 
+    @Override
     public String toString() {
-        return String.format("Author(firstName = \"%s\", lastName = \"%s\")", firstName, lastName);
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 }
--- src/lesson1/Book.java
+++ src/lesson1/Book.java
@@ -1,5 +1,7 @@
 package lesson1;
 
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author author;
@@ -27,7 +29,25 @@ public class Book {
         this.publicationYear = publicationYear;
     }
 
+    @Override
     public String toString() {
-        return String.format("Book(name=\"%s\", author=%s, publicationYear=%s)", name, author, publicationYear);
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author=" + author +
+                ", publicationYear=" + publicationYear +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, publicationYear);
     }
 }
